4ca31412ded07453bd7023fa9ca7880944594b04
Ioan Mert Aydogan <77471284+ioanaydgn@users.noreply.github.com>
2022-12-06 08:47:52 +0200

    logo change

---
+++ src/styles/Colors.tsx
@@ -0,0 +1,11 @@
+export const myColors = {
+    light: '#F1F2F3',
+    dark: '#17171C',
+    blue: '#4B5EFC',
+    btnGray: '#4E505F',
+    btnDark: '#2E2F38',
+    gray: '#727277',
+    black: '#181818',
+    white: '#F2F2F2',
+    result: '#46D5B2',
+}
